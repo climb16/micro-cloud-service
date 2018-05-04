e0ef7204d32c524b32c83a5e4f70d1e629179bc1
guolixiao <guolixiao@xianlai-inc.com>
2018-05-04 15:13:16 +0800

    add spring cloud config mode

---
+++ micro-cloud-server-config/src/main/java/online/jfree/micro/cloud/server/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package online.jfree.micro.cloud.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @description:
+ * @author: Guo Lixiao
+ * @date 2018/5/3 16:10
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
