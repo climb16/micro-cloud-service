435129a52bd7897d140f46e0a71e8c9d98ef49e9
guolixiao <guolixiao@xianlai-inc.com>
2018-05-08 09:44:30 +0800

    service-user服务注册配置

---
+++ micro-cloud-server-zuul/src/main/java/online/jfree/micro/cloud/server/zuul/ZuulServerApplication.java
@@ -0,0 +1,20 @@
+package online.jfree.micro.cloud.server.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @description: ${todo}
+ * @author: Guo Lixiao
+ * @date 2018/5/8 9:30
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
