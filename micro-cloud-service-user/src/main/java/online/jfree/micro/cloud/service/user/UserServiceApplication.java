b102f2e6ac8f60100761a720cacf3dfd3cf2ebfc
guolixiao <guolixiao@xianlai-inc.com>
2018-05-17 18:36:24 +0800

    spring cloud 升级到 Finchley.RC1 spring boot 升级到 2.0.2.RELEASE

--- micro-cloud-service-user/src/main/java/online/jfree/micro/cloud/service/user/UserServiceApplication.java
+++ micro-cloud-service-user/src/main/java/online/jfree/micro/cloud/service/user/UserServiceApplication.java
@@ -3,6 +3,7 @@ package online.jfree.micro.cloud.service.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
 /**
  * @description:
